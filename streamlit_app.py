de4b8f88fd7d4b974da739f8ceb59e41caeb5629
DaviddelaOsaSDG <167071443+DaviddelaOsaSDG@users.noreply.github.com>
2025-02-26 16:04:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-session = get_active_session()
+#session = get_active_session()
 
 # Write directly to the app
 st.title(":tropical_drink: Customize Your Smoothie! :tropical_drink:")
